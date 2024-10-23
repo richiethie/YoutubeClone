36c83b3e7dda9c18b4fc8480d2735073de904ac3
Richie Thiesfeldt <rthiesfeldt@roydan.com>
2024-10-23 16:15:28 -0500

    created video grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
 import PageHeader from './components/PageHeader'
 import CategoryPills from './components/CategoryPills'
-import { categories } from './data/home'
+import { categories, videos } from './data/home'
 import { useState } from 'react'
+import VideoGridItem from './components/VideoGridItem'
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0])
@@ -14,6 +15,11 @@ function App() {
         <div className='overflow-x-hidden px-8 pb-4'>
           <div className='sticky top-0 ng0white z-10 pb-4'>
             <CategoryPills categories={categories} selectedCategory={selectedCategory} onSelect={setSelectedCategory} />
+          </div>
+          <div className='grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]'>
+            {videos.map(video => (
+              <VideoGridItem key={video.id} {...video} />
+            ))}
           </div>
         </div>
       </div>
